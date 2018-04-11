f84d7b8385cc2f15324a8e0ad625e52d8bd68ad9
Srinivas kalyan <srinivaskalyani12@gmail.com>
2018-04-11 18:17:45 +0530

    Signal for the signup

--- restrict/models.py
+++ restrict/models.py
@@ -1,5 +1,9 @@
 from django.db import models
+from django.dispatch import receiver
 from django.contrib.auth.models import User
+from allauth.account.signals import user_signed_up
+
+from organizations.models import Organization, UserOrganization
 
 
 class RestrictInfrastructure(models.Model):
@@ -8,3 +12,11 @@ class RestrictInfrastructure(models.Model):
 
     def __str__(self):
         return self.title
+
+@receiver(user_signed_up)
+def user_signed_up_(request, user, **kwargs):
+    organization = Organization(name = str(user.username)+"_organization")
+    organization.save()
+
+    user_organization = UserOrganization(user = user,organization = organization)
+    user_organization.save()
